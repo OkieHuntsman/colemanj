94f3fe78eb43b3dd98de6461c802c886f52845db
OkieHuntsman <46794724+OkieHuntsman@users.noreply.github.com>
2019-02-21 10:53:47 -0500

    TypingGame

---
+++ TypingGameColemanj/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WordGenerator : MonoBehaviour {
+
+    private static string[] wordList = {"cout<<","cin>>","<iostream>","#include","{}","[]","main()","int","float","double","return0","using",
+    "namespace","std",";","for()","while()","<fstream>",".open",".close","bool","<vector>","<ctime>","char","const","i++","i--","ofstream","void",
+    "outputFile<<","++num","endl","\n","\t","num1/num2","(x>y)","else","average","display(5)","static","<string>","<iomanip>","cows",".length",":",".width","file",
+    "case","<cctype>","switch" };
+
+    public static string GetRandomWord ()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+
+
+
+}
