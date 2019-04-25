be3248d8a8d1ec14d9b890cf9b618967d3f173e0
OkieHuntsman <46794724+OkieHuntsman@users.noreply.github.com>
2019-04-25 16:46:04 -0400

    update

---
+++ Assignment7FNinjaR/Assets/Scripts/Knife.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public GameObject bladeTrailPrefab;
+
+    bool isCutting = false;
+
+    GameObject currentBladeTrail;
+
+    Rigidbody2D rb;
+
+    Camera cam;
+
+    CircleCollider2D circleCollider;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void UpdateCut()
+    {
+        rb.position = cam.ScreenToWorldPoint(Input.mousePosition);
+        //21:50
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollider.enabled = true;
+    }
+
+
+    void StopCutting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2f);
+        circleCollider.enabled = false;
+    }
+}
